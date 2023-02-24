5df5149febe3900c22f4bfdc968ee43e541f69a2
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2023-02-24 10:31:42 -0500

    Update part1reducer.py

--- part1reducer.py
+++ part1reducer.py
@@ -8,16 +8,16 @@ from collections import defaultdict
 dict_ip = defaultdict(int)
 
 for line in sys.stdin:
-line = line.strip()
-ip, num = line.split('\t')
-try:
-num = int(num)
-dict_ip[ip] += num
+  line = line.strip()
+  ip, num = line.split('\t')
+  try:
+    num = int(num)
+    dict_ip[ip] += num
 
-except ValueError:
-pass
+  except ValueError:
+    pass
 
 sorted_ip = sorted(dict_ip.items(), key=itemgetter(0))
 
 for ip, count in sorted_ip:
-print('%s\t%s' % (ip, count))
+  print('%s\t%s' % (ip, count))
