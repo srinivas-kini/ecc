1c83e177e6e9c08f44fdedccf293bdd960f8590f
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2023-02-24 10:27:22 -0500

    Create part1mapper.py

---
+++ part1mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import logging
+
+pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+
+for line in sys.stdin:
+match = pat.search(line)
+if match:
+print ('%s\t%s' % (match.group('hour')+ ' ' + match.group('ip'), 1))
