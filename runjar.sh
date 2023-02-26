e49c33172a0ada1e00e24399b0262f57f4921db5
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2023-02-26 18:30:00 -0500

    Update runjar.sh

--- runjar.sh
+++ runjar.sh
@@ -9,12 +9,12 @@ chmod 777 $SCRIPTS_DIR/$MAPPER $SCRIPTS_DIR/$REDUCER
 # Run streaming job
 hadoop jar $STREAMING \
  -D mapreduce.job.reduces=1 \
- -input "input/sample.log" \
- -output p1output \
- -mapper "python3 ${MAPPER}" \
- -reducer "python3 ${REDUCER}" \
- -file "${SCRIPTS_DIR}/${MAPPER}" \
- -file "${SCRIPTS_DIR}/${REDUCER}" \
+-input "input/sample.log" \
+-output p1output \
+-mapper "python3 ${MAPPER}" \
+-reducer "python3 ${REDUCER}" \
+-file "${SCRIPTS_DIR}/${MAPPER}" \
+-file "${SCRIPTS_DIR}/${REDUCER}" \
 
 # Print output
 hadoop fs -cat /user/exouser/p1output/part-00000 > $HADOOP_HOME/bin/output.txt
