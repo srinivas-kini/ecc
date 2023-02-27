8e3c6d2e57c63446b33ef0773b631c06d3a4cf82
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2023-02-27 10:11:18 -0500

    Create part2reducer2.py

---
+++ part2reducer2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import heapq
+import json
+
+start, end = sys.argv[1].split('-')
+start, end = int(start), int(end)
+
+heap = []
+
+for line in sys.stdin:
+  line = line.strip()
+  data = json.loads(line)
+  hour = int(data['hour'])
+
+  if hour in (start, end):
+    heapq.heappush(heap, (data['count'], data['ip'], data['hour']))
+
+
+for count, ip, hour in heapq.nlargest(3, heap):
+  print(f'Hour: {hour}, IP: {ip}, Count: {count}')
